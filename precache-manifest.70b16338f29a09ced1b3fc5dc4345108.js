6f05c6020c6c37f7c55f7c64b201552100c73209
harshmewada <harshmewada056@gmail.com>
2020-05-11 00:30:45 +0530

    Updates

---
+++ precache-manifest.70b16338f29a09ced1b3fc5dc4345108.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a62c17846a59a410edc49023693d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48dca687b601bd69f104",
+    "url": "/static/css/main.80370142.chunk.css"
+  },
+  {
+    "revision": "efd91723cf11c17fcac9",
+    "url": "/static/js/2.698acaf3.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.698acaf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48dca687b601bd69f104",
+    "url": "/static/js/main.77aa8985.chunk.js"
+  },
+  {
+    "revision": "7aafce13f85f27ec499b",
+    "url": "/static/js/runtime-main.9d3170d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
